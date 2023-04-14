db605751e6b29f205dcb16aff483124248ebf1fb
Anuar Santoyo <anuar.santoyoalum@campus.tu-berlin.de>
2023-04-14 10:27:19 +0800

    Moved DAQ measurements functions to utils.py and commented them

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import serial
 import numpy as np
 import urllib.request
 from matplotlib.patches import Ellipse
+import time
+import struct
+import pandas as pd
 
 
 def command_robot(solution):
@@ -12,11 +15,12 @@ def command_robot(solution):
     :return: None
     """
     amplitude, frequency = solution
-    amplitude = 90*amplitude  # denormalization
-    frequency = 5*frequency  # denormalization
+    amplitude = 90 * amplitude  # denormalization
+    frequency = 5 * frequency  # denormalization
     print(f"Sending command to batbot amp: {amplitude}, freq: {frequency}...")
     html = urllib.request.urlopen(f"http://192.168.43.246/${amplitude},{frequency}$")
     print("Sent!")
+
 
 def fitness(measurements, plot=False):
     """
@@ -28,7 +32,7 @@ def fitness(measurements, plot=False):
     soll = lambda x: -0.68 * np.sin(0.00255 * x)
     goal = soll(time)
     error = goal - values
-    if plot==True:
+    if plot == True:
         plt.title("Best performing test from this generation")
         plt.plot(goal, label='Goal')
         plt.plot(values, label='measurements')
@@ -41,8 +45,7 @@ def fitness(measurements, plot=False):
 
 
 def min_max_creator(min_value=210, max_value=970):  # Values experimentally calculated
-    return lambda x: (2 / (max_value-min_value)) * x + 1 - 2 * max_value / (max_value-min_value)
-
+    return lambda x: (2 / (max_value - min_value)) * x + 1 - 2 * max_value / (max_value - min_value)
 
 
 def get_measurements(ser):
@@ -51,7 +54,6 @@ def get_measurements(ser):
     determined amount of time. The data is then passed through serial communication and is further processed.
     :return: np.array with the shape nxm, where n is amount of data and m parameters (being the first one a timestamp).
     """
-
 
     ser.write('t'.encode())  # Send command to arduino to start measuring
     print("Measuring...")
@@ -62,11 +64,10 @@ def get_measurements(ser):
 
     array[:, 1] = np.interp(array[:, 1], (210, 970), (-1, +1))
     # linear equation to convert into -1,1 range
-    #array[:, 1] = min_max(array[:, 1])
+    # array[:, 1] = min_max(array[:, 1])
 
     array = np.unique(array, axis=0)  # Remove duplicates, arduino sometimes gets several data in the same millisecond
     return [array[:, 0], array[:, 1]]
-
 
 
 def command_get_measurements(x):
@@ -103,7 +104,7 @@ def get_cov_ellipse(cov, centre, nstd, **kwargs):
     eigvals, eigvecs = eigvals[order], eigvecs[:, order]
 
     # The anti-clockwise angle to rotate our ellipse by
-    vx, vy = eigvecs[:,0][0], eigvecs[:,0][1]
+    vx, vy = eigvecs[:, 0][0], eigvecs[:, 0][1]
     theta = np.arctan2(vy, vx)
 
     # Width and height of ellipse to draw
@@ -112,5 +113,77 @@ def get_cov_ellipse(cov, centre, nstd, **kwargs):
                    angle=np.degrees(theta), **kwargs)
 
 
+def read_measurements_raw(port='/dev/ttyUSB0', duration=10):
+    """
+    Send command to BSQ-JN-P8 (DAQ) to return float measurements of all the sensors connected to it, and give back a list
+    with the raw byte message with a timestamp of the measurement.
+
+    :param port: path to the port the DAQ is connected to, which can be found with python -m serial.tools.list_ports
+    :param duration: Duration of the measurements in seconds
+    :return: A list of tuples in the form of [(timestamp 1, measurement 1), ...]
+    """
+    # Initialize serial connection. Parameters found from DAQ documentation.
+    ser = serial.Serial(port=port, baudrate=115200, bytesize=8, parity='N', stopbits=1)
+    measurements = []
+    start = time.time()
+    while time.time() - start < duration:  # Get measurements as long as the elapse time is smaller than the duration
+        ser.write(b'\x01\x03\x00\xc8\x00\x10\xc5\xf8')  # Send command to DAQ to get floats of all channels
+        # (from DAQ doc)
+        measurements.append((time.time(), ser.read(37)))  # Create list of measurements.
+        # I specified 37 as those are the bytes expected as my response, this allows me to have a higher sample
+        # frequency as always only 37 bytes are read instead of readings getting mixed.
+    ser.close()
+    return measurements[1:]  # Omit first as it usually is a zero.
 
 
+def bytes_to_float(h1, h2, h3, h4):
+    """
+    Convert bytes to float
+    :param h1: first byte
+    :param h2: second byte
+    :param h3: third byte
+    :param h4: fourth byte
+    :return: float number of the given bytes
+    """
+    ba = bytearray()
+    ba.append(h1)
+    ba.append(h2)
+    ba.append(h3)
+    ba.append(h4)
+    return struct.unpack("!f", ba)[0]
+
+
+def measurements_to_df(measurement_bytes):
+    """
+    From list of measurements given by read_measurements_raw(), convert the values in float and return a data frame
+    with the columns being: timestamp, sensor_1, ... , sensor_6
+    :param measurement_bytes: measurements list from read_measurement_raw()
+    :return: Dataframe with sensor readings.
+    """
+    dict_list = []  # Dictionaries list that will be converted to pandas Dataframe
+    for ans in measurement_bytes:
+        row = {'timestamp': ans[0]}  # Initialize row dictionary
+        measure = ans[1]  # DAQ response at given timestamp
+        for i in range(6):  # Iterate through the first 6 sensors (the ones being used)
+            pos = 3 + 4 * i  # Initial index of the bytes (from 4 bytes) of the ith sensor.
+            # Convert the 4 bytes from the sensor reading to float and save them in row dict.
+            row[f'sensor_{i + 1}'] = bytes_to_float(measure[pos], measure[pos + 1], measure[pos + 2], measure[pos + 3])
+        dict_list.append(row)  # Append row dict to list
+    return pd.DataFrame(dict_list)  # Convert from list to dataframe
+
+
+def read_measurements_df(port='/dev/ttyUSB0', duration=10, calibration=True):
+    """
+    Obtain measurements of the DAQ for a given duration and process them into a dataframe.
+    :param port: path to the port the DAQ is connected to, which can be found with python -m serial.tools.list_ports
+    :param duration: Duration of the measurements in seconds
+    :param calibration: Boolean stating if a calibration step should be taken, or data should be returned uncalibrated.
+    :return: Dataframe with sensor readings.
+    """
+    if calibration:  # TODO: Must recheck calibration idea, as when Batbot is mounted it already has a force that
+        # can't be calibrated to zero.
+        shift = measurements_to_df(read_measurements_raw(port=port, duration=1)).drop('timestamp', axis=1).mean()
+        shift['timestamp'] = 0.0
+    else:
+        shift = 0
+    return measurements_to_df(read_measurements_raw(port=port, duration=duration)) - shift
