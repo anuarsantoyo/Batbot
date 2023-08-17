6922645ed282e91dc8c2f0b223d4b9e61d3acb81
Anuar Santoyo <anuar.santoyoalum@campus.tu-berlin.de>
2023-08-17 15:01:21 +0800

    Started working with micropython

--- utils.py
+++ utils.py
@@ -274,7 +274,7 @@ def measurements_to_df(measurement_bytes):
         dict_list.append(row)  # Append row dict to list
     return pd.DataFrame(dict_list)  # Convert from list to dataframe
 
-def read_measurements_df(port='/dev/ttyUSB0', duration=10, calibration=False):
+def read_measurements_df_oldDAQ(port='/dev/ttyUSB0', duration=10, calibration=False):
     """
     Obtain measurements of the DAQ for a given duration and process them into a dataframe.
     :param port: path to the port the DAQ is connected to, which can be found with python -m serial.tools.list_ports
@@ -298,3 +298,39 @@ def read_measurements_df(port='/dev/ttyUSB0', duration=10, calibration=False):
 
 def get_sensor_calibration():
     return pd.read_csv('analysis/sensor_calibration.csv')
+
+def get_one_data(port_obj):
+    port_obj.reset_input_buffer()
+    port_obj.write('r'.encode('ascii'))
+    buf = port_obj.read(72)
+    ret = [float(buf[3:12]),
+           float(buf[15:24]),
+           float(buf[27:36]),
+           float(buf[39:48]),
+           float(buf[51:60]),
+           float(buf[63:72])]
+    return ret
+
+
+def read_measurements_df(port, duration=10, sample_interval_ns=20_000_000):
+    uart = serial.Serial(port, 115200, timeout=1)
+    t_0 = time.time_ns()
+    ret_df = pd.DataFrame(columns=['sensor_1',
+                                   'sensor_2',
+                                   'sensor_3',
+                                   'sensor_4',
+                                   'sensor_5',
+                                   'sensor_6',
+                                   'timestamp'])
+    for i in range(1, int(duration*1_000_000_000/sample_interval_ns)+1):
+        while time.time_ns() < t_0+i*sample_interval_ns:pass
+        line = get_one_data(uart)
+        line.append(time.time())
+        ret_df.loc[i-1] = line
+    return ret_df
+
+
+
+
+
+
